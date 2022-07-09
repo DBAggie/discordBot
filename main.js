b67574fab4b866ce9dffadf53575ad6ebe88aef5
Justin Harris <justinharris3j@gmail.com>
2022-07-09 13:56:30 -0500

    Added connection code and a Ping/Pong test

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-function main() {
-    
+const { Client, Intents } = require("discord.js");
+require('dotenv').config()
+// The Client and Intents are destructured from discord.js, since it exports an object by default. Read up on destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
+
+function main () {
+    console.log('This is just a test 2');
 }
+
+main()
