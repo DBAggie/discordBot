1cd84a6db62ea0809ac1d217154b73771c3c60ef
Justin Harris <justinharris3j@gmail.com>
2022-07-09 15:12:50 -0500

    modified:   .gitignore 	modified:   main.js

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
+const { channel } = require("diagnostics_channel");
 const { Client, Intents } = require("discord.js");
 require('dotenv').config()
 // The Client and Intents are destructured from discord.js, since it exports an object by default. Read up on destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
-
 client.login(process.env.TOKEN);
+const prefix = ".";
 
-function main () {
-    console.log('This is just a test 2');
+function main() {
+    client.on("ready", () => {
+        console.log("I am ready!");
+        
+      });
+
+    client.on("messageCreate", (message) => {
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+        if (message.content.startsWith(`${prefix}ping`)) {
+            message.channel.send("pong!");
+        }
+    })
 }
 
 main()
