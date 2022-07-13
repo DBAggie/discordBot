8313637b4dff7d59b8e4c65898f31ba863b3c685
Justin Harris <justinharris3j@gmail.com>
2022-07-12 22:16:01 -0500

    Updating Interact script

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,25 +1,19 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction, client) {
-		client.on('interactionCreate', async interaction => {
-			console.log(`hit the interaction create script`)
-			if (!interaction.isCommand()) return console.log('Not a valid command');
-
-			const command = client.commands.get(interaction.commandName);
-
-			if (!command) return;
-
-			try {
-				console.log('Trying to execute')
-				await command.execute(interaction);
-			} catch (error) {
-				await interaction.reply({
-					content: 'There was an error while executing this command!',
-					ephemeral: true
-				});
-			}
-			console.log('Below the catch');
-		});
-		//console.log('Below the below catch message');
-	}
+    name: "interactionCreate",
+    async execute(interaction, client) {
+        console.log(`hit the interaction create script`);
+        if (!interaction.isCommand()) return console.log("Not a valid command");
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
+        try {
+            console.log("Trying to execute");
+            await command.execute(interaction);
+        } catch (error) {
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+        console.log("Below the catch");
+    },
 };
